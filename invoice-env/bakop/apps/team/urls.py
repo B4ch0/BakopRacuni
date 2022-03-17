412687e470727748fb5d9c8fe97f89cb724d85bb
B4ch0 <moj@email.com>
2022-03-17 22:22:41 +0100

    detalji na racunu

---
+++ invoice-env/bakop/apps/team/urls.py
@@ -0,0 +1,14 @@
+
+  
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from .views import TeamViewSet
+
+router = DefaultRouter()
+router.register("teams", TeamViewSet, basename="teams") #registrira APi (api/v1/teams)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
